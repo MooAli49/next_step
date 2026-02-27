a8d730cb26843a896bd4ae51633bfa9774d38438
DESKTOP-7DKI6PN\Mohamed <ma8510007@gmail.com>
2026-02-27 16:36:02 +0200

    Add AppSecret class for Supabase configuration

---
+++ lib/core/constants/app_secret.dart
@@ -0,0 +1,7 @@
+class AppSecret {
+  AppSecret._();
+
+  static const String supabaseUrl = 'https://cygiwbzhbqqgxeecttvh.supabase.co';
+  static const String supabaseAnonKey =
+      'sb_publishable_pH1svAewfWJnKwHTyU7s5w_KclQf4z-';
+}
